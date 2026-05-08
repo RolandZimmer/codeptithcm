14aee8bdf633c992e31ff02e9afab946f5bc7410
Nguyễn Toàn <n25dece070@student.ptithcm.edu.vn>
2026-05-09 00:54:08 +0700

    Update 4.cpp

--- 4.cpp
+++ 4.cpp
@@ -1,16 +1,37 @@
 #include <iostream>
 using namespace std;
+#include <cmath>
+bool Isbalanced(int x){
+    int evencount=0;
+    int oddcount=0;
+    while(x>0){
+        int digit=x%10;
+        if(digit%2==0){
+            evencount++;
+        }
+        else {
+            oddcount++;
+        }
+        x=x/10;
+    }
+    return evencount==oddcount;
+}
 
 int main() {
     cout<<"Input_your_number: ";
     int n;
     cin>>n;
-    int sum=0;
-    int factorial=1;
-    for(int i=1;i<=n;i++){
-            factorial=factorial*i;
-            sum=sum+factorial;
+    int count=0;
+    int start=pow(10,n-1);
+    int end=pow(10,n)-1;
+    for(int i=start;i<=end;i++){
+        if(Isbalanced(i)){
+            cout<< i <<" ";
+            count++;
+            if(count%10==0){
+                cout<<endl;
+            }
+        }
     }
-    cout<<sum;
     return 0;
 }
